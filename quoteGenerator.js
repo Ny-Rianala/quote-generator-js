46d21abdac3560a8161fabd4f030aadaf778a3cb
Katiuscia Fanomezantsoa <kati.fan@onja.org>
2020-06-05 12:43:08 +0300

    removing the for loop

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,22 +1,24 @@
 //Create arrays for all quotes
-  // const quoteNumber = prompt("How many quotes (from 1 to 5):");
-  let quote = [
-    ['Thanks God',
+function quoteArr(){
+  let quote1 = [
+    'Thanks God',
     'God loves us',
-    'Always pray'],
-  [
+    'Always pray'
+  ];
+  let randomIndex1= Math.floor(Math.random() *quote1.length);
+  let quote2 = [
     'Please', 
     'Thanks', 
     'Welcome'
-  ],
-  [
+  ];
+  let randomIndex2 = Math.floor(Math.random() *quote2.length);
+  let quote3 = [
       'Fine',
      'Good', 
      'Goodbye'
-  ]
   ];
-// Create a function
-function quotes(){
-  let randQuote = Math.floor(Math.random() * randQuote.length);
-  console.log(quotes);
+  let randomIndex3 = Math.floor(Math.random() *quote3.length);
+  return finalQuote = `${quote1[randomIndex1]} ${quote2[randomIndex2]} ${quote3[randomIndex3]}`;
 }
+  console.log(quoteArr());
+//  const quoteNumber = prompt("How many quotes (from 1 to 5):");
